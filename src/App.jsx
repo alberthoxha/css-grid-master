c0ecc46b3712df5ae595ced02a6d41cad691ca92
Albert Hoxha <123065766+alberthoxha@users.noreply.github.com>
2023-07-24 21:28:21 +0200

    starter-file

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+
+function App() {
+  
+  return (
+    <>
+       <div className="container">
+    <div className="item">1</div>
+    <div className="item">2</div>
+    <div className="item">3</div>
+    <div className="item">4</div>
+    <div className="item">5</div>
+    <div className="item">6</div>
+    <div className="item">7</div>
+    <div className="item">8</div>
+    <div className="item">9</div>
+    <div className="item">10</div>
+  </div>
+
+    </>
+  )
+}
+
+export default App
