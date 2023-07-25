30e9e96841e316721b681b709df01a0ab41273e1
Albert Hoxha <123065766+alberthoxha@users.noreply.github.com>
2023-07-25 05:52:27 +0200

    sizing grid items

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
       <div className="container">
         <div className="item">1</div>
         <div className="item">2</div>
-        <div className="item">Grid</div>
+        <div className="item">3</div>
         <div className="item">4</div>
         <div className="item">5</div>
         <div className="item">6</div>
