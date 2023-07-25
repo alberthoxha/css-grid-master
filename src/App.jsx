8acfaecacdca71a7232dd21a7fc3e0494971ec87
Albert Hoxha <123065766+alberthoxha@users.noreply.github.com>
2023-07-25 07:09:18 +0200

    grid template area

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,6 @@ function App() {
         <div className="item item2">2</div>
         <div className="item item3">3</div>
         <div className="item item4">4</div>
-        <div className="item item5">5</div>
-        <div className="item item6">6</div>
-        <div className="item item7">7</div>
-        <div className="item item8">8</div>
-        <div className="item item9">9</div>
-        <div className="item item10">10</div>
-        <div className="item item11">11</div>
-        <div className="item item12">12</div>
-        <div className="item item13">13</div>
-        <div className="item item14">14</div>
-        <div className="item item15">15</div>
-        <div className="item item16">16</div>
-        <div className="item item17">17</div>
-        <div className="item item18">18</div>
-        <div className="item item19">19</div>
-        <div className="item item20">20</div>
       </div>
     </>
   );
