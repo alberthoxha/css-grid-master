1cc7687a5cf4d40f72decd71566317efe67ae1eb
Albert Hoxha <123065766+alberthoxha@users.noreply.github.com>
2023-07-27 00:34:25 +0200

    Nesting Grid with Album Layouts

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,46 @@ function App() {
         <div className="item item28">28</div>
         <div className="item item29">29</div>
         <div className="item item30">30</div>
+        <div className="item item31">31</div>
+        <div className="item item32">32</div>
+        <div className="item item33">33</div>
+        <div className="item item34">34</div>
+        <div className="item item35">35</div>
+        <div className="item item36">36</div>
+        <div className="item item37">37</div>
+        <div className="item item38">38</div>
+        <div className="item item39">39</div>
+        <div className="item item40">40</div>
+        <div className="item item41">41</div>
+        <div className="item item42">42</div>
+        <div className="item item43">43</div>
+        <div className="item item44">44</div>
+        <div className="item item45">45</div>
+        <div className="item item46">46</div>
+        <div className="item item47">47</div>
+        <div className="item item48">48</div>
+        <div className="item item49">49</div>
+        <div className="item item50">50</div>
+        <div className="item item51">51</div>
+        <div className="item item52">52</div>
+        <div className="item item53">53</div>
+        <div className="item item54">54</div>
+        <div className="item item55">55</div>
+        <div className="item item56">56</div>
+        <div className="item item57">57</div>
+        <div className="item item58">58</div>
+        <div className="item item59">59</div>
+        <div className="item item60">60</div>
+        <div className="item item61">61</div>
+        <div className="item item62">62</div>
+        <div className="item item63">63</div>
+        <div className="item item64">64</div>
+        <div className="item item65">65</div>
+        <div className="item item66">66</div>
+        <div className="item item67">67</div>
+        <div className="item item68">68</div>
+        <div className="item item69">69</div>
+        <div className="item item70">70</div>
       </div>
     </>
   );
