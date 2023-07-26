934bb2f4415687b776dedac1024ecea6ec244ddb
Albert Hoxha <123065766+alberthoxha@users.noreply.github.com>
2023-07-26 23:25:22 +0200

    Naming Lines in CSS Grid

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,19 @@ function App() {
   return (
     <>
       <div className="container">
-        <div className="item item1">1</div>
-        <div className="item item2">2</div>
-        <div className="item item3">3</div>
-        <div className="item item4">4</div>
+        <div className="item item1">
+          <p>I'm Sidebar #1</p>
+        </div>
+        <div className="item item2">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, sed.</p>
+          <p>Lorem ipsum d</p>
+        </div>
+        <div className="item item3">
+          <p>I'm Sidebar #2</p>
+        </div>
+        <div className="item footer">
+          <p>I'm the footer</p>
+        </div>
       </div>
     </>
   );
